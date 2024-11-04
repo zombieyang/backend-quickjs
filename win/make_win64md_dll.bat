d858b9f3bd2d230ccbf0925c7ddbd304b2e5b034
zombieyang <zombieyang@tencent.com>
2024-11-04 19:55:05 +0800

    ..

--- win/make_win64md_dll.bat
+++ win/make_win64md_dll.bat
@@ -1,6 +1,6 @@
 copy /y ..\build\libquickjs.dll.a ..\build\libquickjs.dll.lib
 mkdir build & pushd build
-cmake -DMD=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DWIN_DLL=1 -G "Visual Studio 16 2019" -A x64 ..
+cmake -DMD=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DWIN_DLL=1 -G "Visual Studio 17 2022" -A x64 ..
 popd
 cmake --build build --config Release
 pause
