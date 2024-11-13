a7eab9dbcffa20fdf48e88dc483fa5dc8759af7f
zombieyang <zombieyang@tencent.com>
2024-11-13 17:57:05 +0800

    .

--- quickjs/quickjs.c
+++ quickjs/quickjs.c
@@ -106,7 +106,7 @@ struct timeval
 /* dump objects freed by the garbage collector */
 //#define DUMP_GC_FREE
 /* dump objects leaking when freeing the runtime */
-//#define DUMP_LEAKS  1
+#define DUMP_LEAKS  1
 /* dump memory usage before running the garbage collector */
 //#define DUMP_MEM
 //#define DUMP_OBJECTS    /* dump objects in JS_FreeContext */
